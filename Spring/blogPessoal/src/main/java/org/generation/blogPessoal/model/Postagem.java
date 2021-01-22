9488e46ef36696fa466bf5ab61a59ab7fa2c83eb
DaviVieira2210 <davi.vieira205@gmail.com>
2021-01-22 08:23:29 -0300

    Update blogPessoal

--- Spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ Spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -4,11 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagem")
@@ -29,8 +32,11 @@ public class Postagem
     @Temporal(TemporalType.TIMESTAMP)
     private Date date = new java.sql.Date(System.currentTimeMillis());
     
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Tema tema;   
     
-    public long getId() {
+	public long getId() {
         return id;
     }
     public void setId(long id) {
@@ -54,4 +60,10 @@ public class Postagem
     public void setDate(Date date) {
         this.date = date;
     }
+    public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 }
